77f71e78f8479fbc1f0341872fb606801a86e087
JohnNPhillips <j.phillips@email.com>
2014-03-30 23:57:55 -0400

    By default, don't print out debug log messages

--- app/src/com/ayros/historycleaner/helpers/Logger.java
+++ app/src/com/ayros/historycleaner/helpers/Logger.java
@@ -1,33 +1,37 @@
 package com.ayros.historycleaner.helpers;
 
-
 public class Logger
 {
+	private static final boolean DEBUG_MODE = false;
+
 	public static void error(String message)
 	{
 		System.err.println("ERROR: " + message);
 	}
-	
+
 	public static void errorST(String message)
 	{
 		error(message);
 		printStack();
 	}
-	
+
 	public static void errorST(String message, Exception e)
 	{
 		error(message);
 		System.err.println("Inner Exception: " + e.toString());
-		
+
 		System.err.print("Stack Trace: ");
 		e.printStackTrace();
 	}
-	
+
 	public static void debug(String message)
 	{
-		System.out.println("DEBUG: " + message);
+		if (DEBUG_MODE)
+		{
+			System.out.println("DEBUG: " + message);
+		}
 	}
-	
+
 	public static void printStack()
 	{
 		Thread.dumpStack();
